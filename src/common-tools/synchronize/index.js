e9b08a84f674892be8725c0e6eed5dfd37cd7ac8
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-04-21 17:00:06 +0200

    Call synchronization of Queen

--- src/common-tools/synchronize/index.js
+++ src/common-tools/synchronize/index.js
@@ -1,5 +1,11 @@
-export const synchronizeQueen = () => {
+const synchronizeQueen = () => {
   const data = { type: 'PEARL', command: 'SYNCHRONIZE' };
   const event = new CustomEvent('PEARL', { detail: data });
   window.dispatchEvent(event);
+};
+
+export const synchronize = async () => {
+  synchronizeQueen();
+
+  // TODO : pearl synch
 };
