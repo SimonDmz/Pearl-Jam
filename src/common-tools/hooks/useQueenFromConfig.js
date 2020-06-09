7a552ad4175b6a6f94aa0c7c85542762cd435065
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-06-09 16:59:20 +0200

    update useQueen

--- src/common-tools/hooks/useQueenFromConfig.js
+++ src/common-tools/hooks/useQueenFromConfig.js
@@ -1,33 +1,13 @@
-import { useEffect } from 'react';
-
 const useQueenFromConfig = url => {
-  useEffect(() => {
-    const scripts = [];
-    const importQueenScript = async () => {
-      const response = await fetch(url);
-      const { QUEEN_URL } = await response.json();
-
-      window.localStorage.setItem('QUEEN_URL', QUEEN_URL);
-
-      const manifest = await fetch(`${QUEEN_URL}/asset-manifest.json`);
-      const { entrypoints } = await manifest.json();
-
-      entrypoints.forEach(scriptUrl => {
-        if (scriptUrl.endsWith('.js')) {
-          const script = document.createElement('script');
-          script.src = `${QUEEN_URL}/${scriptUrl}`;
-          script.async = true;
-          document.body.appendChild(script);
-          scripts.push(script);
-        }
-      });
-    };
-    importQueenScript();
-
-    return () => {
-      scripts.forEach(scriptElement => document.body.removeChild(scriptElement));
-    };
-  }, [url]);
+  const importQueenScript = async () => {
+    const response = await fetch(url);
+    let { QUEEN_URL } = await response.json();
+    QUEEN_URL = 'http://localhost:5000';
+    const script = document.createElement('script');
+    script.src = `${QUEEN_URL}/entry.js`;
+    document.body.appendChild(script);
+  };
+  importQueenScript();
 };
 
 export default useQueenFromConfig;
