3b4f5e31b0485e784d7e94366dcfbbd86b7d654e
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-06-10 15:40:45 +0200

    update useQueenFromConfig

--- src/common-tools/hooks/useQueenFromConfig.js
+++ src/common-tools/hooks/useQueenFromConfig.js
@@ -1,13 +1,15 @@
-const useQueenFromConfig = url => {
-  const importQueenScript = async () => {
-    const response = await fetch(url);
-    let { QUEEN_URL } = await response.json();
-    QUEEN_URL = 'http://localhost:5000';
+import { useEffect } from 'react';
+
+export const useQueenFromConfig = url => {
+  const importQueenScript = async configurationUrl => {
+    const response = await fetch(configurationUrl);
+    const { QUEEN_URL } = await response.json();
     const script = document.createElement('script');
     script.src = `${QUEEN_URL}/entry.js`;
     document.body.appendChild(script);
   };
-  importQueenScript();
-};
 
-export default useQueenFromConfig;
+  useEffect(() => {
+    importQueenScript(url);
+  }, [url]);
+};
