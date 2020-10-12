cd51b4d7979b00bc8357719ede31edf7fa364143
Demazière Simon <simon.demaziere@insee.fr>
2020-10-12 10:51:26 +0200

    fix ANS-> PRC on UePage, fix WFT -> WFS when clicking Transmit, WIP survey-unit state management

--- src/common-tools/constants/index.js
+++ src/common-tools/constants/index.js
@@ -1,3 +1,6 @@
+import surveyUnitStateEnum from 'common-tools/enum/SUStateEnum';
+import contactAttemptEnum from 'common-tools/enum/ContactAttemptEnum';
+
 export const KEYCLOAK = 'keycloak';
 export const ANONYMOUS = 'anonymous';
 export const AUTHENTICATION_MODE_ENUM = [ANONYMOUS, KEYCLOAK];
@@ -11,3 +14,32 @@ export const GUEST_PEARL_USER = {
   roles: ['Guest'],
 };
 export const JSON_UTF8_HEADER = 'application/json;charset=utf-8';
+
+export const PREVIOUS_STATES_TO_BE_KEEPED = [
+  surveyUnitStateEnum.WAITING_FOR_TRANSMISSION.type,
+  surveyUnitStateEnum.QUESTIONNAIRE_STARTED.type,
+];
+
+export const CONTACT_RELATED_STATES = [
+  surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type,
+  surveyUnitStateEnum.APPOINTMENT_MADE.type,
+];
+
+export const CONTACT_SUCCESS_LIST = [
+  contactAttemptEnum.INTERVIEW_ACCEPTED.type,
+  contactAttemptEnum.APPOINTMENT_MADE.type,
+];
+
+export const STATES_ACCEPTING_ANY_NEW_STATE = [
+  surveyUnitStateEnum.IN_PREPARATION.type,
+  surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type,
+  surveyUnitStateEnum.APPOINTMENT_MADE.type,
+];
+
+export const STATES_UPDATING_TO_WFT = [
+  surveyUnitStateEnum.WAITING_FOR_TRANSMISSION.type,
+  surveyUnitStateEnum.WAITING_FOR_SYNCHRONIZATION.type,
+  surveyUnitStateEnum.TO_BE_REVIEWED.type,
+  surveyUnitStateEnum.FINALIZED.type,
+  surveyUnitStateEnum.QUESTIONNAIRE_NOT_AVAILABLE.type,
+];
--- src/common-tools/functions/surveyUnitFunctions.js
+++ src/common-tools/functions/surveyUnitFunctions.js
@@ -1,5 +1,12 @@
 import surveyUnitDBService from 'indexedbb/services/surveyUnit-idb-service';
 import contactAttemptDBService from 'indexedbb/services/contactAttempt-idb-service';
+import {
+  CONTACT_RELATED_STATES,
+  CONTACT_SUCCESS_LIST,
+  STATES_ACCEPTING_ANY_NEW_STATE,
+  STATES_UPDATING_TO_WFT,
+} from 'common-tools/constants';
+import surveyUnitStateEnum from 'common-tools/enum/SUStateEnum';
 
 export const getCommentByType = (type, ue) => {
   if (Array.isArray(ue.comments) && ue.comments.length > 0) {
@@ -9,16 +16,21 @@ export const getCommentByType = (type, ue) => {
 };
 
 export const getLastState = ue => {
-  if (Array.isArray(ue.states) && ue.states.length > 0) {
+  if (Array.isArray(ue.states) && ue.states.length === 1) return ue.states[0];
+  if (Array.isArray(ue.states) && ue.states.length > 1) {
     return ue.states.reduce((a, b) => (a.date > b.date ? a : b));
   }
   return false;
 };
 
 export const isValidForTransmission = ue => {
-  //const { contactOutcome } = ue;
+  const { contactOutcome } = ue;
+  return contactOutcome !== null;
+};
 
-  return true;
+const getContactAttempts = async surveyUnit => {
+  const { contactAttempts } = surveyUnit;
+  return contactAttemptDBService.findByIds(contactAttempts);
 };
 
 export const deleteContactAttempt = (surveyUnit, contactAttemptId) => {
@@ -30,11 +42,111 @@ export const deleteContactAttempt = (surveyUnit, contactAttemptId) => {
   contactAttemptDBService.delete(contactAttemptId);
 };
 
+const getContactAttemptNumber = surveyUnit => {
+  return surveyUnit.states.filter(
+    state => state.type === surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type
+  ).length;
+};
+
+const lastContactAttemptIsSuccessfull = surveyUnit => {
+  const contactAttempts = getContactAttempts(surveyUnit);
+  const lastAOCstate = contactAttempts.reduce((a, b) => (a.date > b.date ? a : b));
+  return CONTACT_SUCCESS_LIST.includes(lastAOCstate.status);
+};
+
+const isContactAttemptOk = surveyUnit => {
+  return lastContactAttemptIsSuccessfull(surveyUnit);
+};
+
+const addContactState = (surveyUnit, newState) => {
+  // this method add AOC or APS state in states if needed => for life cycle consistency
+  // rule #1 if you add AOC and AOC is successfull : should add APS then
+  // ruel #2 if you add APS : sould add AOC before if there is no AOC in states
+
+  // switch between AOC | APS (from newState.type)
+  switch (newState.type) {
+    case surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type:
+      // add AOC state
+      surveyUnit.states.push(newState);
+      if (isContactAttemptOk(surveyUnit)) {
+        // add APS then
+        surveyUnit.states.push({
+          date: new Date().getTime(),
+          type: surveyUnitStateEnum.APPOINTMENT_MADE.type,
+        });
+      }
+      break;
+
+    case surveyUnitStateEnum.APPOINTMENT_MADE.type:
+      if (getContactAttemptNumber(surveyUnit) === 0) {
+        // add AOC state with 1 ms earlier date
+        surveyUnit.states.push({
+          date: newState.date - 1,
+          type: surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type,
+        });
+        // add APS state
+        surveyUnit.states.push(newState);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return surveyUnit;
+};
+
 export const addNewState = async (surveyUnit, stateType) => {
+  const lastStateType = getLastState(surveyUnit).type;
   const newState = { date: new Date().getTime(), type: stateType };
-  const newSu = surveyUnit;
+  let newSu = surveyUnit;
+
+  // case management : lastState * addedState
+  switch (lastStateType) {
+    // INS then adding AOC or APS should also finally add INS
+    case surveyUnitStateEnum.QUESTIONNAIRE_STARTED.type:
+      if (CONTACT_RELATED_STATES.includes(stateType)) {
+        newSu = addContactState(newSu, newState);
+
+        const previousState = { date: new Date().getTime(), type: lastStateType };
+        newSu.states.push(previousState);
+      } else {
+        newSu.states.push(newState);
+      }
+      break;
+
+    case STATES_ACCEPTING_ANY_NEW_STATE:
+      if (CONTACT_RELATED_STATES.includes(stateType)) {
+        newSu = addContactState(newSu, newState);
+      } else {
+        newSu.states.push(newState);
+      }
+      break;
+    // WFT/WFS/TBR/FIN/QNA then adding
+    case STATES_UPDATING_TO_WFT:
+      // TODO : peut-être d'autres états  gérer ici
+      if (CONTACT_RELATED_STATES.includes(stateType)) {
+        newSu = addContactState(newSu, newState);
+        newSu.states.push({
+          date: new Date().getTime(),
+          type: surveyUnitStateEnum.WAITING_FOR_TRANSMISSION.type,
+        });
+      } else {
+        newSu.states.push(newState);
+      }
+      break;
+    default:
+      break;
+  }
+
+  // old version :to be deleted
   newSu.states.push(newState);
   newSu.lastState = newState;
+
+  // keep function ending : clean
   newSu.selected = false;
   await surveyUnitDBService.addOrUpdate(newSu);
+};
+
+export const checkIfContactAttemptCanBeDeleted = surveyUnit => {
+  return getContactAttemptNumber(surveyUnit) > 1;
 };
