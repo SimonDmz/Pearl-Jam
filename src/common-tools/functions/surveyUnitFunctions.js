b15f07307ad357f5bc37fda824bca08a3be2d2ed
Demazière Simon <simon.demaziere@insee.fr>
2020-10-14 10:20:19 +0200

    fix asynchronous indexedDB calls

--- src/common-tools/functions/surveyUnitFunctions.js
+++ src/common-tools/functions/surveyUnitFunctions.js
@@ -43,32 +43,26 @@ const getContactAttemptNumber = surveyUnit => {
   ).length;
 };
 
-const lastContactAttemptIsSuccessfull = surveyUnit => {
-  const contactAttempts = getContactAttempts(surveyUnit);
-  const lastAOCstate = contactAttempts.reduce((a, b) => (a.date > b.date ? a : b));
-  return CONTACT_SUCCESS_LIST.includes(lastAOCstate.status);
+const lastContactAttemptIsSuccessfull = async surveyUnit => {
+  const contactAttempts = await getContactAttempts(surveyUnit);
+  let lastContactAttempt;
+  if (Array.isArray(contactAttempts) && contactAttempts.length > 1) {
+    lastContactAttempt = contactAttempts.reduce((a, b) => (a.date > b.date ? a : b));
+  } else {
+    lastContactAttempt = contactAttempts;
+  }
+  return CONTACT_SUCCESS_LIST.includes(lastContactAttempt.status);
 };
 
-const isContactAttemptOk = surveyUnit => {
+const isContactAttemptOk = async surveyUnit => {
   return lastContactAttemptIsSuccessfull(surveyUnit);
 };
 
-const addContactState = (surveyUnit, newState) => {
-  // this method add AOC or APS state in states if needed => for life cycle consistency
-  // rule #1 if you add a successfull AOC : should add APS if there is no APS in states
-  // ruel #2 if you add APS : sould add AOC before if there is no AOC in states
-  console.log('------------');
-  console.log(surveyUnit.states);
-
-  // switch between AOC | APS (from newState.type)
+const addContactState = async (surveyUnit, newState) => {
   switch (newState.type) {
     case surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type:
-      // add AOC state
-      console.log('add AOC');
       surveyUnit.states.push(newState);
-      if (isContactAttemptOk(surveyUnit) && surveyUnit.contactOutcome !== null) {
-        // add APS then
-        console.log('contactOK so add APS after');
+      if (await isContactAttemptOk(surveyUnit)) {
         surveyUnit.states.push({
           date: new Date().getTime(),
           type: surveyUnitStateEnum.APPOINTMENT_MADE.type,
@@ -78,21 +72,17 @@ const addContactState = (surveyUnit, newState) => {
 
     case surveyUnitStateEnum.APPOINTMENT_MADE.type:
       if (getContactAttemptNumber(surveyUnit) === 0) {
-        // add AOC state with 1 ms earlier date
-        console.log('should add AOC before');
         surveyUnit.states.push({
           date: newState.date - 1,
           type: surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type,
         });
-        // add APS state
-        console.log('add APS');
-        surveyUnit.states.push(newState);
       }
+      surveyUnit.states.push(newState);
       break;
     default:
+      console.log('erreur avec le type : ', newState.type);
       break;
   }
-  console.log(surveyUnit.states);
   return surveyUnit;
 };
 
@@ -100,15 +90,10 @@ export const addNewState = async (surveyUnit, stateType) => {
   const lastStateType = getLastState(surveyUnit).type;
   const newState = { date: new Date().getTime(), type: stateType };
   let newSu = surveyUnit;
-  console.log('ADD NEW STATE');
-  console.log('lastStateType = ', lastStateType, ' | stateType = ', stateType);
-  // case management : lastState * addedState
   switch (lastStateType) {
-    // INS then adding AOC or APS should also finally add INS
     case surveyUnitStateEnum.QUESTIONNAIRE_STARTED.type:
-      console.log('case INS');
       if (CONTACT_RELATED_STATES.includes(stateType)) {
-        newSu = addContactState(newSu, newState);
+        newSu = await addContactState(newSu, newState);
 
         const previousState = { date: new Date().getTime(), type: lastStateType };
         newSu.states.push(previousState);
@@ -118,24 +103,28 @@ export const addNewState = async (surveyUnit, stateType) => {
       break;
 
     case surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type:
-      console.log('case AOC');
       if (surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type === stateType) {
+        if (await isContactAttemptOk(surveyUnit)) {
+          surveyUnit.states.push({
+            date: new Date().getTime(),
+            type: surveyUnitStateEnum.APPOINTMENT_MADE.type,
+          });
+        }
         break;
       }
       if (surveyUnitStateEnum.APPOINTMENT_MADE.type === stateType) {
-        newSu = addContactState(newSu, newState);
+        newSu = await addContactState(newSu, newState);
       } else {
         newSu.states.push(newState);
       }
       break;
 
     case surveyUnitStateEnum.APPOINTMENT_MADE.type:
-      console.log('case APS');
       if (surveyUnitStateEnum.APPOINTMENT_MADE.type === stateType) {
         break;
       }
       if (surveyUnitStateEnum.AT_LEAST_ONE_CONTACT.type === stateType) {
-        newSu = addContactState(newSu, newState);
+        newSu = await addContactState(newSu, newState);
       } else {
         newSu.states.push(newState);
       }
@@ -144,24 +133,21 @@ export const addNewState = async (surveyUnit, stateType) => {
     case surveyUnitStateEnum.IN_PREPARATION.type:
     case surveyUnitStateEnum.VISIBLE_NOT_CLICKABLE.type:
     case surveyUnitStateEnum.VISIBLE_AND_CLICKABLE.type:
-      console.log('case any new state');
       if (CONTACT_RELATED_STATES.includes(stateType)) {
-        newSu = addContactState(newSu, newState);
+        newSu = await addContactState(newSu, newState);
       } else {
         newSu.states.push(newState);
       }
       break;
 
-    // WFT/WFS/TBR/FIN/QNA then adding
     case surveyUnitStateEnum.WAITING_FOR_TRANSMISSION.type:
     case surveyUnitStateEnum.WAITING_FOR_SYNCHRONIZATION.type:
     case surveyUnitStateEnum.TO_BE_REVIEWED.type:
     case surveyUnitStateEnum.FINALIZED.type:
     case surveyUnitStateEnum.QUESTIONNAIRE_NOT_AVAILABLE.type:
-      console.log('case updating to WFT if contact related state added');
       // TODO : peut-être d'autres états  gérer ici
       if (CONTACT_RELATED_STATES.includes(stateType)) {
-        newSu = addContactState(newSu, newState);
+        newSu = await addContactState(newSu, newState);
         newSu.states.push({
           date: new Date().getTime(),
           type: surveyUnitStateEnum.WAITING_FOR_TRANSMISSION.type,
@@ -174,7 +160,7 @@ export const addNewState = async (surveyUnit, stateType) => {
       console.log('default case nothing done');
       break;
   }
-
+  console.log('new state is ', getLastState(await newSu).type);
   newSu.selected = false;
   await surveyUnitDBService.addOrUpdate(newSu);
 };
