a93c5c69f4b420d8a406d8207e434448f6d48a96
Simon Demazière <simon.demaziere@insee.fr>
2020-11-03 17:05:15 +0100

    remove console.logs

--- src/common-tools/functions/surveyUnitFunctions.js
+++ src/common-tools/functions/surveyUnitFunctions.js
@@ -106,13 +106,11 @@ export const addNewState = async (surveyUnit, stateType) => {
   switch (lastStateType) {
     case surveyUnitStateEnum.QUESTIONNAIRE_STARTED.type:
       if (CONTACT_RELATED_STATES.includes(stateType)) {
-        console.log('contact related state added');
         newSu = await addContactState(newSu, newState);
 
         const previousState = { date: new Date().getTime(), type: lastStateType };
         newSu.states.push(previousState);
       } else {
-        console.log('add state not related to contacts');
         newSu.states.push(newState);
       }
       break;
@@ -175,10 +173,8 @@ export const addNewState = async (surveyUnit, stateType) => {
       console.log('default case nothing done');
       break;
   }
-  console.log('new state is ', getLastState(await newSu).type);
   newSu.selected = false;
   await surveyUnitDBService.addOrUpdate(newSu);
-  console.log('updated SU : ', newSu);
 };
 
 export const checkIfContactAttemptCanBeDeleted = surveyUnit => {
